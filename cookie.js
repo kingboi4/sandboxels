20e9a3af5a6ee7bc2073c52177a93664ba5b353c
kingboi4 <155130792+kingboi4@users.noreply.github.com>
2023-12-29 07:21:16 -0600

    Create cookie.js
    gives u cokies

---
+++ cookie.js
@@ -0,0 +1,18 @@
+// Adding elements:
+elements.cookie = {
+    color: "#eee0b1",
+    behavior: behaviors.LIQUID,
+    category: "food",
+    viscosity: 900000,
+    state: "solid",
+    density: 720,
+};
+// Adding elements:
+elements.cookie doght = {
+    color:"E7D3C6",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 1000000,
+    state: "liquid",
+    density: 720,
+};
